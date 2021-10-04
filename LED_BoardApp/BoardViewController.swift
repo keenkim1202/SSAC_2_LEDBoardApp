6a4c0b70032e22b3193d0e3d4cc9e3a6d450121b
KEEN <homurahomu@naver.com>
2021-10-04 16:42:18 +0900

    스토리보드 화면 구성

---
+++ LED_BoardApp/BoardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BoardViewController.swift
+//  LED_BoardApp
+//
+//  Created by KEEN on 2021/10/04.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# SSAC_2_LEDBoardApp
-LED 전광판앱입니다. 입력한 텍스트를 LED전광판처럼 크게 출력해 줍니다.
