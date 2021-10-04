cd8fa3d3bb88f704e0ec9a936eceab67c36d5104
KEEN <homurahomu@naver.com>
2021-10-04 16:58:59 +0900

    UI configure

--- LED_BoardApp/BoardViewController.swift
+++ LED_BoardApp/BoardViewController.swift
@@ -8,11 +8,52 @@
 import UIKit
 
 class BoardViewController: UIViewController {
-
+  // MARK: UI
+  @IBOutlet weak var resultLabel: UILabel!
+  @IBOutlet weak var userTextField: UITextField!
+  @IBOutlet weak var userInputView: UIView!
+  @IBOutlet weak var sendButton: UIButton!
+  @IBOutlet weak var textColorButton: UIButton!
+  
+  // MARK: View Life-Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    configure()
   }
-
-
+  
+  // MARK: Configure
+  func configure() {
+    // button text
+    if let sendLabel = sendButton.titleLabel {
+      sendLabel.text = "보내기"
+      sendLabel.textColor = .black
+    }
+    
+    if let textColorLabel = textColorButton.titleLabel {
+      textColorLabel.text = "Aa"
+      textColorLabel.textColor = .red
+    }
+    
+    // button border
+    setBorder([sendButton, textColorButton])
+    
+    // user InputView
+    userInputView.layer.cornerRadius = CGFloat(8)
+  }
+  
+  func setBorder(_ buttons: [UIButton]) {
+    for bt in buttons {
+      bt.layer.borderColor = UIColor.black.cgColor
+      bt.layer.borderWidth = CGFloat(2)
+      bt.layer.cornerRadius = CGFloat(8)
+    }
+  }
+ 
+  // MARK: Actions
+  @IBAction func onSend(_ sender: UIButton) {
+  }
+  
+  @IBAction func onTextColorChange(_ sender: UIButton) {
+  }
+  
 }
-
